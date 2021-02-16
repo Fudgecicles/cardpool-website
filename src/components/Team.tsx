4a7dc4a154d7dd835768098de172fa7ff68c513f
Steven Li <listeven@usc.edu>
2021-02-15 17:57:31 -0800

    componentized team member

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -6,6 +6,7 @@ import lain from "../assets/lain.jpg";
 import Colors from "../config/Colors";
 import WaveTop from "./WaveTop";
 import WaveBottom from "./WaveBottom";
+import TeamMember from "./TeamMember";
 
 const CenteredText = styled.h2`
   padding-top: 50px;
@@ -17,31 +18,11 @@ const TeamBackground = styled.div`
   background-color: ${Colors.blue};
 `;
 
-const MemberImage = styled.img<ColorProp>`
-  border-width: 10px;
-  border-style: solid;
-  border-color: ${(props) => props.backgroundColor};
-  border-radius: 100%;
-  margin-bottom: 25px;
-  width: 300px;
-`;
-
 const TeamLayout = styled.div`
   margin-top: 50px;
   display: flex;
   justify-content: space-evenly;
 `;
-
-const MemberContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  align-items: center;
-`;
-
-type ColorProp = {
-  backgroundColor: string;
-};
 
 function Team() {
   return (
@@ -51,20 +32,30 @@ function Team() {
         <Container>
           <CenteredText>An award winning team</CenteredText>
           <TeamLayout>
-            <MemberContainer>
-              <MemberImage backgroundColor={Colors.card} src={steven} />
-              <h3>Steven Li</h3>
-              <h4>University of Southern California</h4>
-              <h4>Sledgehammer Games</h4>
-              <h4>PAX 10 Featured Developer</h4>
-            </MemberContainer>
-            <MemberContainer>
-              <MemberImage backgroundColor={Colors.pool} src={lain} />
-              <h3>McLean Goldwhite</h3>
-              <h4>University of Southern California</h4>
-              <h4>Pinscreen</h4>
-              <h4>BAFTA Ones to Watch 2016</h4>
-            </MemberContainer>
+            <TeamMember
+              borderColor={Colors.card}
+              image={steven}
+              name="Steven Li"
+              body={
+                <>
+                  <h4>University of Southern California </h4>
+                  <h4>Sledgehammer Games</h4>
+                  <h4>PAX 10 2015</h4>
+                </>
+              }
+            />
+            <TeamMember
+              borderColor={Colors.pool}
+              image={lain}
+              name="McLean Goldwhite"
+              body={
+                <>
+                  <h4>University of Southern California </h4>
+                  <h4>Pinscreen</h4>
+                  <h4>BAFTA Ones to Watch 2016</h4>
+                </>
+              }
+            />
           </TeamLayout>
         </Container>
       </TeamBackground>
---
+++ src/components/TeamMember.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import react, { FunctionComponent } from "react";
+
+const MemberImage = styled.img<ColorProp>`
+  border-width: 10px;
+  border-style: solid;
+  border-color: ${(props) => props.backgroundColor};
+  border-radius: 100%;
+  margin-bottom: 25px;
+  width: 300px;
+`;
+
+const MemberContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+`;
+
+type ColorProp = {
+  backgroundColor: string;
+};
+
+type TeamProps = {
+  borderColor: string;
+  name: string;
+  body: React.ReactNode;
+  image: string;
+};
+
+const TeamMember: FunctionComponent<TeamProps> = (props) => {
+  return (
+    <MemberContainer>
+      <MemberImage backgroundColor={props.borderColor} src={props.image} />
+      <h3>{props.name}</h3>
+      {props.body}
+    </MemberContainer>
+  );
+};
+
+export default TeamMember;
